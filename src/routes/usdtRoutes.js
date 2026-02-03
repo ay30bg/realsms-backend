7f24ce52a9c739a03d2e4c4fa2c21295b258e00a
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-03 15:20:11 +0100

    Update usdtRoutes.js

--- src/routes/usdtRoutes.js
+++ src/routes/usdtRoutes.js
@@ -18,8 +18,10 @@ const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+console.log("protect:", protect);
+console.log("initUSDTPayment:", initUSDTPayment);
+
 router.post("/init", protect, initUSDTPayment);
 router.post("/webhook", usdtWebhook);
 
 module.exports = router;
-
