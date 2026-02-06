3c2826106a4e3215ed987100b68f614dccd96735
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-06 17:36:12 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,3 @@
-// // models/User.js
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     firstName: { type: String, required: true },
-//     lastName: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-
-//     // 🔑 Forgot password
-//     resetPasswordToken: String,
-//     resetPasswordExpire: Date,
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", userSchema);
-
-
 // models/User.js
 import mongoose from "mongoose";
 
@@ -39,3 +19,4 @@ const userSchema = new mongoose.Schema(
 );
 
 export default mongoose.model("User", userSchema);
+
