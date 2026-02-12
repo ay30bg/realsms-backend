058b2360e4dc3d50cb506a10831f094f1a5fb8fa
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-12 14:00:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const opayRoutes = require('./routes/opayRoutes');
 const usdtRoutes = require('./routes/usdtRoutes');
 const walletRoutes = require('./routes/walletRoutes');
+const paystackRoutes = require('./routes/paystack');
 
 const app = express();
 
@@ -47,6 +48,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/opay', opayRoutes);
 app.use('/api/usdt', usdtRoutes); 
 app.use('/api/wallet', walletRoutes);
+app.use('/api/paystack/webhook', express.raw({ type: '*/*' }));
 
 // ================= MONGODB =================
 mongoose
@@ -76,6 +78,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
