ea084ca0df799dcd9240585c405d55581a406a6d
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-17 15:35:32 +0100

    Update orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { buyNumber, getOtp } = require("../controllers/order");
+const { buyNumber, getOtp } = require("../controllers/orderController");
 
 router.post("/buy", buyNumber);
 router.post("/otp", getOtp);
