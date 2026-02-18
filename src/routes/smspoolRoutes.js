2a68c18b5ca8a52407975c5d85394092dd23ecab
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-18 15:44:10 +0100

    Update smspoolRoutes.js

--- src/routes/smspoolRoutes.js
+++ src/routes/smspoolRoutes.js
@@ -1,21 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getServers,
-//   getServices,
-//   buyNumber,
-//   getOtp,
-// } = require("../controllers/smsController");
-// const { protect } = require("../middleware/authMiddleware");
-
-// // ---------------- ROUTES ----------------
-// router.get("/servers", protect, getServers);
-// router.get("/services/:serverId", protect, getServices);
-// router.post("/buy", protect, buyNumber);
-// router.get("/otp/:number", protect, getOtp);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const {
