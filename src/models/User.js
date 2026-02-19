51b1c4210e6fb82e24b57ff52c10ee8d08e4641b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-19 10:18:05 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,50 +1,3 @@
-// // models/User.js
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     firstName: { type: String, required: true },
-//     lastName: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-
-//     // 🔑 Forgot password
-//     resetPasswordToken: String,
-//     resetPasswordExpire: Date,
-
-//     // 💰 Wallet balance
-//     walletBalance: { type: Number, default: 0 },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", userSchema);
-
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     firstName: { type: String, required: true },
-//     lastName: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-
-//     // 🔑 Forgot password
-//     resetPasswordToken: String,
-//     resetPasswordExpire: Date,
-
-//     // 💰 Wallet balance (NAIRA)
-//     walletBalanceNGN: {
-//       type: Number,
-//       default: 0,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", userSchema);
-
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -66,3 +19,4 @@ const userSchema = new mongoose.Schema(
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
