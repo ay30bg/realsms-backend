97e1a6defc18ad6fc60181c7cdb7dba90394efbb
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-21 07:04:33 +0100

    Update transactionsRoutes.js

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,12 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { getUserTransactionStats } = require("../controllers/transactionController");
-
-// // GET /api/transactions/stats/:userId
-// router.get("/stats/:userId", getUserTransactionStats);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const { getUserTransactionStats } = require("../controllers/transactionController");
