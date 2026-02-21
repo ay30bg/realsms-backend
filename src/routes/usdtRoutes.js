8d458f907ae8a10d04d0b3c22efbf7082ed3e583
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-21 07:04:48 +0100

    Update usdtRoutes.js

--- src/routes/usdtRoutes.js
+++ src/routes/usdtRoutes.js
@@ -1,17 +1,3 @@
-// // routes/usdtRoutes.js
-// const express = require('express');
-// const { initUSDTPayment, usdtWebhook } = require('../controllers/usdtController');
-
-// const router = express.Router();
-
-// // POST /api/usdt/init
-// router.post('/init', initUSDTPayment);
-
-// // POST /api/usdt/webhook
-// router.post('/webhook', usdtWebhook);
-
-// module.exports = router; // ✅ make sure to export router
-
 const express = require("express");
 const { initUSDTPayment, usdtWebhook } = require("../controllers/usdtController");
 const { protect } = require("../middleware/authMiddleware");
