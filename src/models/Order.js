ae57cc4af98edb6eda00f339b3162df430f80d22
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-21 07:03:06 +0100

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,31 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const orderSchema = new mongoose.Schema(
-//   {
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     service: String,
-//     country: String,
-//     orderid: String,
-//     number: String,
-//     baseCost: Number,
-//     priceCharged: Number,
-//     profit: Number,
-//     status: {
-//       type: String,
-//       enum: ["waiting", "received", "cancelled"],
-//       default: "waiting",
-//     },
-//     otp: String,
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Order", orderSchema);
-
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
