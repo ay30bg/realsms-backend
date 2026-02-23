36cb5de2b3bcf39b017f3aadc5f926b37df7d2bc
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-02-23 11:55:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const usdtRoutes = require('./routes/usdtRoutes');
 const walletRoutes = require('./routes/walletRoutes');
 const paystackRoutes = require("./routes/paystackRoutes");
 const korapayRoutes = require("./routes/korapayRoutes");
+const flutterwaveRoutes = require("./routes/flutterwaveRoutes");
 const smspoolRoutes = require("./routes/smspoolRoutes");
 const transactionRoutes = require("./routes/transactionsRoutes");
 
@@ -52,6 +53,7 @@ app.use('/api/usdt', usdtRoutes);
 app.use('/api/wallet', walletRoutes);
 app.use("/api/paystack", paystackRoutes); 
 app.use("/api/korapay", korapayRoutes);
+app.use("/api/flutterwave", flutterwaveRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/smspool", smspoolRoutes);
 
@@ -83,6 +85,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
