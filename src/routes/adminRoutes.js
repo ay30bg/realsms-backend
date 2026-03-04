00aec6e4d38dc319fa77afb0dbc0390c829eeee8
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-04 13:17:57 +0100

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,20 @@
+// const express = require("express");
+// const router = express.Router();
+// const { getAdminStats } = require("../controllers/adminController");
+// const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
+
+// router.get("/stats", protect, adminOnly, getAdminStats);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
-const { getAdminStats } = require("../controllers/adminController");
 const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
+const adminController = require("../controllers/adminController");
 
-router.get("/stats", protect, adminOnly, getAdminStats);
+router.get("/stats", protect, adminOnly, adminController.getAdminStats);
+router.get("/users", protect, adminOnly, adminController.getAllUsers);
+router.get("/transactions", protect, adminOnly, adminController.getAllTransactions);
+router.get("/orders", protect, adminOnly, adminController.getAllOrders);
 
 module.exports = router;
