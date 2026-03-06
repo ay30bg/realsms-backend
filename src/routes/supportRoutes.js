8310279c5f2a0278ea84c1007e47afea565c106a
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-06 22:43:41 +0100

    Create supportRoutes.js

---
+++ src/routes/supportRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  sendMessage,
+  adminReply,
+  getUserMessages,
+  getAdminMessages,
+} = require("../controllers/supportController");
+
+const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
+
+
+// user send message
+router.post("/send", protect, sendMessage);
+
+// user fetch messages
+router.get("/user", protect, getUserMessages);
+
+// admin fetch all chats
+router.get("/admin", protect, adminOnly, getAdminMessages);
+
+// admin reply
+router.post("/reply", protect, adminOnly, adminReply);
+
+module.exports = router;
