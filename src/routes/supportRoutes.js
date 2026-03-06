dc2cbfc333fd0b91a3e2d3bc27d344ef037005f3
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-06 23:11:27 +0100

    Update supportRoutes.js

--- src/routes/supportRoutes.js
+++ src/routes/supportRoutes.js
@@ -1,32 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   sendMessage,
-//   adminReply,
-//   getUserMessages,
-//   getAdminMessages,
-// } = require("../controllers/supportController");
-
-// const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
-// // const { protectUser } = require("../middleware/authMiddleware");
-
-
-// // user send message
-// router.post("/send", protect, sendMessage);
-
-// // user fetch messages
-// router.get("/user", protect, getUserMessages);
-
-// // admin fetch all chats
-// router.get("/admin", protect, adminOnly, getAdminMessages);
-
-// // admin reply
-// router.post("/reply", protect, adminOnly, adminReply);
-
-// module.exports = router;
-
-
 const express = require("express");
 const router = express.Router();
 
@@ -38,20 +9,21 @@ const {
 } = require("../controllers/supportController");
 
 const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
-const { protect } = require("../middleware/authMiddleware");
+// const { protectUser } = require("../middleware/authMiddleware");
 
-// ---------------- User Routes ---------------- //
-// User sends message
+
+// user send message
 router.post("/send", protect, sendMessage);
 
-// User fetch messages
+// user fetch messages
 router.get("/user", protect, getUserMessages);
 
-// ---------------- Admin Routes ---------------- //
-// Admin fetch all conversations
+// admin fetch all chats
 router.get("/admin", protect, adminOnly, getAdminMessages);
 
-// Admin reply to user
+// admin reply
 router.post("/reply", protect, adminOnly, adminReply);
 
 module.exports = router;
+
+
