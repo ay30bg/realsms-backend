c42864b85c4754f07adaa573ab1e463a8dc9f9e0
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-06 22:42:16 +0100

    Create supportController.js

---
+++ src/controllers/supportController.js
@@ -0,0 +1,61 @@
+const Support = require("../models/Support");
+
+
+// USER SEND MESSAGE
+exports.sendMessage = async (req, res) => {
+  try {
+    const message = await Support.create({
+      user: req.user._id,
+      message: req.body.message,
+      sender: "user",
+    });
+
+    res.json(message);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// ADMIN REPLY
+exports.adminReply = async (req, res) => {
+  try {
+    const message = await Support.create({
+      user: req.body.userId,
+      message: req.body.message,
+      sender: "admin",
+    });
+
+    res.json(message);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// GET USER CHAT
+exports.getUserMessages = async (req, res) => {
+  try {
+    const messages = await Support.find({
+      user: req.user._id,
+    }).sort({ createdAt: 1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// ADMIN GET ALL CONVERSATIONS
+exports.getAdminMessages = async (req, res) => {
+  try {
+    const messages = await Support.find()
+      .populate("user", "email")
+      .sort({ createdAt: -1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
